fe9bd138c31f4f130189115d01e7762a608591d6
Raiza Mythic <raiza@mythic.xyz>
2023-12-20 17:42:51 +0700

    update case in ascdescsortverwsi 2

--- apiMythRepo/dev/ascdesSortv2.js
+++ apiMythRepo/dev/ascdesSortv2.js
@@ -2,6 +2,7 @@ const axios = require('axios');
 const { expect } = require('chai');
 const { describe } = require('mocha');
 var PropertiesReader = require("properties-reader");
+const supertest = require('supertest');
 var properties = PropertiesReader("config/env.properties");
 const baseURL = properties.get("devURI");
 const createURL = (path) => `${baseURL}${path}`;
@@ -30,7 +31,7 @@ describe('Ascending and Descending Mythic Files', async() => {
         console.log(unconMythAsc.data.data.mythic);
         expect(unconMythAsc.status).to.equal(200);
     });
-    it('Descending in Confirmed Mythic Files', async() => {
+    it('Descending in Unconfirmed Mythic Files', async() => {
         const unconMythDesc = await axios.get(createURL('unconfirmed/mythic?sortBy=3'))
         console.log('Data from Mythic: ');
         const mythicData = unconMythDesc?.data?.mythic;
@@ -48,7 +49,7 @@ describe('Ascending and Descending Casefiles', async() => {
         console.log(conCasfAsc.data.data.casefile);
         expect(conCasfAsc.status).to.equal(200);
     });
-    it('Descending in Confirmed Mythic Files', async() => {
+    it('Descending in Confirmed Casefile', async() => {
         const conCasfDesc = await axios.get(createURL('confirmed/casefile?sortBy=3'))
         console.log('Data from Casefile: ');
         const casefileData = conCasfDesc?.data?.casefile;
@@ -64,11 +65,22 @@ describe('Ascending and Descending Casefiles', async() => {
         console.log(unconCaseAsc.data.data.casefile);
         expect(unconCaseAsc.status).to.equal(200);
     });
-    it('Descending in Confirmed Casefile', async() => {
+    it.only('Descending in unconfirmed Casefile', async() => {
         const unconCaseDesc = await axios.get(createURL('unconfirmed/casefile?sortBy=3'))
         console.log('Data from Casefile: ');
         const mythicData = unconCaseDesc?.data?.casefile;
         console.log(unconCaseDesc.data.data.casefile);
         expect(unconCaseDesc.status).to.equal(200);
+
+
+        /* Verify, ensure that the Data is Unconfirmed, isConfirmed: false*/
+        const unconCaseDescs = supertest(createURL('unconfirmed/casefile?sortBy=3'));
+        const responses = await unconCaseDescs.get(''); 
+        responses.body.data.casefile.forEach(unconCaseDesc => {
+            console.log("Ensure that the Data isConfirmed: false \n" + "actual --> " + unconCaseDesc.code + " " + unconCaseDesc.isConfirmed);
+            expect(unconCaseDesc.isConfirmed).to.be.false;
+        });
     });
 });
+
+//console.log('Verify, ensure that the Data is Unconfirmed, isConfirmed: false');
